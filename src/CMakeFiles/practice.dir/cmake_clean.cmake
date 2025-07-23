91b2c208c3c48d7c8e233c08fd6fa943a32edc8f
Braydon Kirchhefer <160270628+Galahaddx@users.noreply.github.com>
2025-07-23 19:39:13 +0000

    Done with week 10

---
+++ src/CMakeFiles/practice.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practice.dir/practice.cpp.o"
+  "CMakeFiles/practice.dir/practice.cpp.o.d"
+  "practice"
+  "practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
